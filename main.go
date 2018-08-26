080961059fe78908b405d6834f4158ff0518941a
kyle <kyleoldham.4@gmail.com>
2018-08-26 13:55:10 -0600

    feat(server): add server

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"time"
 
 	pb "github.com/mralanlee/hello-world-protobuf/messages"
 )
 
-func main() {
-	fmt.Println("Hello")
+func myHandler(w http.ResponseWriter, req *http.Request) {
 	hello := pb.Hello{
 		World: "World",
 	}
-	fmt.Printf("Hello %s", hello.World)
+	io.WriteString(w, hello.World)
+}
+
+func main() {
+	// Create Server
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        http.HandlerFunc(myHandler),
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	log.Fatal(s.ListenAndServe())
 }
